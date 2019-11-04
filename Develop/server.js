eed0bb06f5e7a866c59c3b3e18cd5a9e2fca4063
kevin-on-davis <kevin.on.davis@gmail.com>
2019-11-03 20:08:05 -0500

    Saving note successful-api calls need resolution

--- Develop/server.js
+++ Develop/server.js
@@ -10,49 +10,36 @@ var notebook = [];
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("./public"));
+app.use(express.static("public"));
+app.use(express.static("db"));
 
 // Basic route that sends the user first to the AJAX Page
-app.get("/note", function (req, res) {
-    // res.sendFile("notes.html");
-    // res.sendFile(path.join(__dirname, '../../notes.html'));
-    res.sendFile("notes.html");
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, '../../index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.get("/api/notes", function (req, res) {
-    return (res.json(notebook));
+    res.sendFile(path.join(__dirname, "/db/db.json"));
 });
 
 app.post("/api/notes", function (req, res) {
     var note = req.body;
-    console.log(note);
-
-    fs.readFile("db.json", function (err, data) {
-        notebook = JSON.parse(res.json(data));
-        console.log(notebook);
-    })
-
-    // notebook.push({id:notebook.length, })
-    if (HR.res_list.length < 5) {
-        HR.res_list.push(newReservation);
-    }
-    else {
-        HR.wait_list.push(newReservation);
-    }
-    res.json(newReservation);
+    fs.readFile(path.join(__dirname, "/db/db.json"), function (err, data) {
+        notebook = JSON.parse(data);
+        req.body.id = notebook.length + 1;
+        notebook.push(req.body)
+        fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(notebook), function (err) {
+            if (err) throw err;
+        });
+    });
 });
 
 app.delete("/api/notes/:id", function (req, res) {
     res.sendFile("index.html");
-});
-
-// Displays all characters
-app.get("/api/characters", function (req, res) {
-    return res.json(characters);
 });
 
 // Starts the server to begin listening
