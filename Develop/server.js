066cf4480bfcf4cca52b2ab693b623e16e29e256
kevin-on-davis <kevin.on.davis@gmail.com>
2019-11-03 23:48:02 -0500

    Filtering the array left to be done

--- Develop/server.js
+++ Develop/server.js
@@ -13,6 +13,10 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(express.static("db"));
 
+app.get("/api/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
 // Basic route that sends the user first to the AJAX Page
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
@@ -22,12 +26,7 @@ app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-app.get("/api/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "/db/db.json"));
-});
-
 app.post("/api/notes", function (req, res) {
-    var note = req.body;
     fs.readFile(path.join(__dirname, "/db/db.json"), function (err, data) {
         notebook = JSON.parse(data);
         req.body.id = notebook.length + 1;
@@ -39,7 +38,16 @@ app.post("/api/notes", function (req, res) {
 });
 
 app.delete("/api/notes/:id", function (req, res) {
-    res.sendFile("index.html");
+    fs.readFile(path.join(__dirname, "/db/db.json"), function (err, data) {
+        notebook = JSON.parse(data);
+        console.log(notebook[2].id);
+        new_array = notebook.filter((note, index) => note[index].id !== req.params.id);
+        console.log("My result", new_array);
+        fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(notebook.filter(note => note.id !== req.params.id)), function (err) {
+            if (err) throw err;
+        });
+
+    });
 });
 
 // Starts the server to begin listening
