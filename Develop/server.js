cd0b66764625da31494a456817cfbbc191f6b5a2
kevin-on-davis <kevin.on.davis@gmail.com>
2019-11-11 11:30:01 -0500

    Pushing to Heroku

--- Develop/server.js
+++ Develop/server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var path = require("path");
 var app = express();
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 var notebook = [];
 
@@ -13,41 +13,50 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(express.static("db"));
 
-app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-app.get("/api/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "/db/db.json"));
+app.get("/api/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "/db/db.json"));
 });
 
 // Basic route that sends the user first to the AJAX Page
-app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-app.post("/api/notes", function (req, res) {
-    fs.readFile(path.join(__dirname, "/db/db.json"), function (err, data) {
-        notebook = JSON.parse(data);
-        req.body.id = notebook.length + 1;
-        notebook.push(req.body)
-        fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(notebook), function (err) {
-            if (err) throw err;
-            console.log(`Added note : ${req.body.title}`);
-        });
-    });
+app.post("/api/notes", function(req, res) {
+  fs.readFile(path.join(__dirname, "/db/db.json"), function(err, data) {
+    notebook = JSON.parse(data);
+    req.body.id = notebook.length + 1;
+    notebook.push(req.body);
+    fs.writeFile(
+      path.join(__dirname, "/db/db.json"),
+      JSON.stringify(notebook),
+      function(err) {
+        if (err) throw err;
+        console.log(`Added note : ${req.body.title}`);
+      }
+    );
+  });
 });
 
-app.delete("/api/notes/:id", function (req, res) {
-    fs.readFile(path.join(__dirname, "/db/db.json"), function (err, data) {
-        notebook = JSON.parse(data);
-        fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(notebook.filter(note => note.id != req.params.id)), () => { console.log(`Deletion of note ${req.params.id} successful`) });
-
-    });
+app.delete("/api/notes/:id", function(req, res) {
+  fs.readFile(path.join(__dirname, "/db/db.json"), function(err, data) {
+    notebook = JSON.parse(data);
+    fs.writeFile(
+      path.join(__dirname, "/db/db.json"),
+      JSON.stringify(notebook.filter(note => note.id != req.params.id)),
+      () => {
+        console.log(`Deletion of note ${req.params.id} successful`);
+      }
+    );
+  });
 });
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
 });
--- README.md
+++ README.md
@@ -1,25 +1,25 @@
 # L2K_NoteTaker
+
 This application's purpose is to write, save, and delete notes. The notes are saved as data in a JSON file which pays the role of a pseudo-database.
 
 The application frontend was prewritten for us with the requirement being to create endpoints to call the html files and save, list and delete notes. The endpoints to be created are:
 
-* The application utilizes a `db.json` file on the backend that will be used to store and retrieve notes using the `fs` module.
+- The application utilizes a `db.json` file on the backend that will be used to store and retrieve notes using the `fs` module.
 
-* The following API routes should be created:
+- The following API routes should be created:
 
+* GET `/notes` - Should return the `notes.html` file.
 
-  * GET `/notes` - Should return the `notes.html` file.
+* GET `*` - Should return the `index.html` file
 
-  * GET `*` - Should return the `index.html` file
+* GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
 
-  * GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+* POST `/api/notes` - Should recieve a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. Each note created is assigned a numeric ID which is populated with the length of an intermediate array which holds the contents of the db.json file.
 
-  * POST `/api/notes` - Should recieve a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. Each note created is assigned a numeric ID which is populated with the length of an intermediate array which holds the contents of the db.json file.
+* DELETE `/api/notes/:id` - Should receive a query paramter containing the id of a note to delete. The note chosen for deletion is removed by using the filter method.
 
-  * DELETE `/api/notes/:id` - Should recieve a query paramter containing the id of a note to delete.  The note chosen for deletion is removed by using the filter method.
-
-  Technology used:
-  Express
-  Postman
-  HTML
-  JSON
+Technology used:
+Express
+Postman
+HTML
+JSON
